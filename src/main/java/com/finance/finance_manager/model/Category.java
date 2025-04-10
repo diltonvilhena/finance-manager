f7e0294e649debaf84e73c0ba997cd6e57a8f9a0
19/05 1.1 <diltonvilhena@gmail.com>
2025-04-09 22:46:13 -0300

    feat: criação entidade Category

---
+++ src/main/java/com/finance/finance_manager/model/Category.java
@@ -0,0 +1,30 @@
+package com.finance.finance_manager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter @Setter @NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "category")
+    private List<Expense> expenses;
+}
